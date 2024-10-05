52722fb88b020cf3980d24afc60431741812d58c
utyagijjwal <utyagi2003@gmail.com>
2024-10-05 21:53:20 +0530

    about working successfull

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         {/* Logo Section */}
         <div className="text-2xl md:text-3xl font-bold text-white">
           <Link to="home" smooth={true} duration={500} className="hover:text-indigo-300 transition duration-300">
-            Ujjwal's Portfolio
+            <span className="text-yellow-500">U</span>jjwal's Portfolio
           </Link>
         </div>
 
